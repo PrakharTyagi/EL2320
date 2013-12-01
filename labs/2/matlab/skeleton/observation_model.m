867c861fd8059747f935a77425ee0108448e5bf2
Michal Staniaszek <m.staniaszek@gmail.com>
2013-12-01 16:00:49 +0100

    observation model completed

--- labs/2/matlab/skeleton/observation_model.m
+++ labs/2/matlab/skeleton/observation_model.m
@@ -8,5 +8,13 @@
 % Outputs:  
 %           h           2XM
 function h = observation_model(S,W,j)
-% FILL IN HERE
+
+xdist = W(1,j)-S(1,:);
+ydist = W(2,j)-S(2,:);
+angle = atan2(ydist,xdist)-S(3,:);
+normangle = mod(angle+pi, 2*pi) - pi;
+
+h = [sqrt(xdist.^2 + ydist.^2); % euclidean distance
+     normangle];
+
 end
